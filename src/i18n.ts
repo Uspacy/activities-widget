9d654331e2e906a6e36f8a26c52e4acf8aceb1c8
MysikM <mishamysik@gmail.com>
2025-03-25 09:46:50 +0200

    change path

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,7 +7,7 @@ import { initReactI18next } from 'react-i18next';
 i18n.use(initReactI18next)
 	.use(
 		resourcesToBackend((language: string, namespace: string, callback) => {
-			import(`./locales/${language}/${namespace}.json`)
+			import(`./locales-widget/${language}/${namespace}.json`)
 				.then((resources) => {
 					console.log(resources, 'resources');
 					callback(null, resources);
